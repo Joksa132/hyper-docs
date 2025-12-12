c8934380951cfa67d036e942f4a6c151b5a45191
Joksa132 <nikolajoksimovic419@gmail.com>
2025-12-12 14:13:19 +0100

    set up drizzle and better auth

---
+++ backend/src/auth.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db/client.js";
+import * as schema from "./db/schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, { provider: "pg", usePlural: true, schema }),
+  secret: process.env.BETTER_AUTH_SECRET!,
+  trustedOrigins: [process.env.FRONTEND_URL!, process.env.BETTER_AUTH_URL!],
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+});
