a908f718d2b73a0c7538803afbc27d56810e505a
Joksa132 <nikolajoksimovic419@gmail.com>
2026-01-29 17:29:56 +0100

    hopefully fix better-auth and ts errors

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,13 +1,23 @@
 import "dotenv/config";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "./db/client.js";
+import { db } from "./db/client";
 import * as schema from "./db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "pg", usePlural: true, schema }),
+  baseURL: process.env.BETTER_AUTH_URL!,
   secret: process.env.BETTER_AUTH_SECRET!,
   trustedOrigins: [process.env.FRONTEND_URL!, process.env.BETTER_AUTH_URL!],
+  advanced: {
+    crossSubDomainCookies: {
+      enabled: true,
+    },
+    defaultCookieAttributes: {
+      secure: true,
+      sameSite: "none",
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
